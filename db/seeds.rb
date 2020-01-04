9713f58e918eaa3db7988c7dfb60b850a0a1a1f7
Ricky Dam <RickyDamOttawa@gmail.com>
2020-01-03 20:32:56 -0500

    Udemy185: Build User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "ricky10", password: "password")
+User.create(username: "ricky11", password: "password")
+User.create(username: "ricky12", password: "password")
+User.create(username: "ricky13", password: "password")
+User.create(username: "ricky14", password: "password")
